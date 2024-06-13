fe2e62a3e68f6846c69440c3d862f0c253aa60d9
MChandanSwarit <chandanswarit2804@gmail.com>
2024-06-13 18:32:07 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,7 +6,6 @@ console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-
   socket.on('data', (data) => {
     const request = data.toString();
 
@@ -15,45 +14,37 @@ const server = net.createServer((socket) => {
     const [method, path] = requestLine.split(' ');
 
     if (method === 'GET') {
-
       if (path === '/') {
-
         socket.write('HTTP/1.1 200 OK\r\n\r\n');
-
       } else if (path.startsWith('/echo/')) {
-
         const content = path.slice(6);
         socket.write(
           `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
         );
-
       } else if (path.startsWith('/user-agent')) {
-
         const content = userAgent.slice(12);
         socket.write(
           `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
         );
-
       } else if (path.startsWith('/files')) {
-
         const directory = process.argv[3];
         const filename = path.slice(7);
 
         if (fs.existsSync(`${directory}/${filename}`)) {
-
-          const content = fs.readFileSync(`${directory}/${filename}`).toString();
+          const content = fs
+            .readFileSync(`${directory}/${filename}`)
+            .toString();
           socket.write(
             `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`
           );
-
-      } else {
-
-        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-
+        } else {
+          socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+        }
       }
-    }
-  }
-});
+    } else {
+      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    };
+  });
 
   socket.on('close', () => {
     socket.end();
