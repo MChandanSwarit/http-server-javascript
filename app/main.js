1e87d6103eefefb4bd4646e140585b14e3a2b665
MChandanSwarit <chandanswarit2804@gmail.com>
2024-06-13 16:19:53 +0530

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -5,10 +5,21 @@ console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on('data', () => {
+  // socket.on('data', () => {
+  //   const response = 'HTTP/1.1 200 OK\r\n\r\n';
+  //   socket.write(response);
+  // });
+
+  const [requestLine] = request.split('\r\n');
+  const [method, path] = requestLine.split(' ');
+
+  if (method === 'GET' && path === '/index.html') {
     const response = 'HTTP/1.1 200 OK\r\n\r\n';
     socket.write(response);
-  });
+  } else {
+    const response = 'HTTP/1.1 404 Not Found\r\n\r\n';
+    socket.write(response);
+  }
 
   socket.on('close', () => {
     socket.end();
