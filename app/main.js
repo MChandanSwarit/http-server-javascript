19fa623afa4d834ade2a70292294e16ea5f1eca9
MChandanSwarit <chandanswarit2804@gmail.com>
2024-06-13 16:09:47 +0530

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -1,13 +1,18 @@
-const net = require("net");
+const net = require('net');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log('Logs from your program will appear here!');
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on('data', () => {
+    const response = 'HTTP/1.1 200 OK\r\n\r\n';
+    socket.write(response);
+  });
+
+  socket.on('close', () => {
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, 'localhost');
