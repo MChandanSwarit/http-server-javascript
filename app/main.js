c92117eaa0e327d11dc4de1e02a12a4c2437b2b1
MChandanSwarit <chandanswarit2804@gmail.com>
2024-06-13 16:41:18 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -13,15 +13,14 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const request = data.toString();
 
-    const str = 'abc';
-
     const [requestLine] = request.split('\r\n');
-    const req = requestLine.split(' ');
+    const [method, path] = requestLine.split(' ');
 
-    if (req[0] === 'GET' && req[1] === '/echo/abc') {
-      const response =
-        'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc';
-      socket.write(response);
+    if (method === 'GET') {
+      if (path.startsWith('/echo/')) {
+        const message = path.substring(6, 9);
+        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n{message}`;
+      }
     } else {
       const response = 'HTTP/1.1 404 Not Found\r\n\r\n';
       socket.write(response);
